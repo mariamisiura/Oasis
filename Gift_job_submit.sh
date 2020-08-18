7499ac56a02ab1d37fd26304082d4d08035b1286
mariamisiura <67112598+mariamisiura@users.noreply.github.com>
2020-08-18 10:45:15 -0400

    Create Gift_job_submit.sh
    This script runs the matlab gift batch files script, which then runs your gift batch file. In total, you will need 3 scripts: job submission, run the batch file script, and the batch file itself. You will need to change the usernames and node configuration based on your project.

---
+++ Gift_job_submit.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#SBATCH --nodes=1
+#SBATCH --ntasks=32
+#SBATCH --time=8-00:15:00     # 8 days and 15 minutes
+#SBATCH --mail-user=mmisiura1@gsu.edu
+#SBATCH --mail-type=ALL
+#SBATCH -p qPSYC
+#SBATCH -e /home/users/mmisiura1/error%A-%a.err
+#SBATCH -o /home/users/mmisiura1/out%A-%a.out
+
+export MODULEPATH=/apps/Compilers/modules-3.2.10/Debug-Build/Modules/3.2.10/modulefiles/
+
+module load Framework/Matlab2016b
+matlab -nodisplay -nosplash < /home/users/mmisiura1/Oasis_run10.m
